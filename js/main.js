e3e19e00a92c38a320ee645e8728c19ae62c0f8e
yonglee <yonglee@student.42seoul.kr>
2021-05-30 21:49:30 +0900

    lecture 17

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener('click', function () {
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '');
+});
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+        // 배지 숨기기
+        // gsap.to(요소, 지속시간, 옵션);
+        gsap.to(badgeEl, 0.6, {
+            opacity: 0,
+            display: 'none',
+        });
+    }
+    else {
+        // 배지 보이기
+        gsap.to(badgeEl, 0.6, {
+            opacity: 1,
+            display: 'block',
+        });
+    }
+}, 300));
+// _.throttle(함수, ms)
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(fadeEl, 1, {
+        delay: 0.7 * (index + 1),
+        opacity: 1,
+    });
+});
