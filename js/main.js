c44d7fc2b99fe2eba9afdfe5f84cf9d6a2a9751a
Yonghun Lee <yonglee@c7r1s1.42seoul.kr>
2021-06-01 19:08:45 +0900

    lecture 29

--- js/main.js
+++ js/main.js
@@ -71,6 +71,19 @@ new Swiper('.promotion .swiper-container', {
     }
 });
 
+new Swiper('.awards .swiper-container', {
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next',
+    }
+})
+
+
+
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
@@ -117,3 +130,4 @@ spyELs.forEach(function (spyEl) {
         .setClassToggle(spyEl, 'show') // 클래스를 추가해줌
         .addTo(new ScrollMagic.Controller());
 })
+
