c84b7a94acf0b8230cc700fe4166915e51ac1be2
yonglee <yonglee@student.42seoul.kr>
2021-06-01 09:00:07 +0900

    lecture 24

--- js/main.js
+++ js/main.js
@@ -45,3 +45,43 @@ fadeEls.forEach(function (fadeEl, index) {
         opacity: 1,
     });
 });
+
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true,
+});
+
+new Swiper('.promotion .swiper-container', {
+    slidesPerView : 3,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    // autoplay:{
+    //     delay: 5000,
+
+    // }
+    pagination : {
+        el: '.promotion .swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next',
+    }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+    isHidePromotion = !isHidePromotion
+    if (isHidePromotion) {
+        // 숨김 처리
+        promotionEl.classList.add('hide');
+
+    } else {
+        // 보임 처리
+        promotionEl.classList.remove('hide');
+    };
+});
